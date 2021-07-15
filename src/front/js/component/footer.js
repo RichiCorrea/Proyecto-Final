de35cd2d59905962fe2c0e1efd9a6b20d378e7ca
RichiCorrea <richi.correa.f@gmail.com>
2021-07-15 21:18:11 +0000

    frase footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			¡Las aventuras esperan que giren tus ruedas! <i className="fa fa-bicycle text-black" /> by{" "}
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
 		</p>
 	</footer>
