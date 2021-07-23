e7e20d0c67e1b7c6710e2afd53b4f0927ec4b0ad
Gabriel <g.valdivia1436@hotmail.com>
2021-07-23 02:13:09 +0000

    map taller

--- src/front/js/pages/MapTaller.js
+++ src/front/js/pages/MapTaller.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../../styles/maptaller.scss";
 import maptaller from "../../img/maptaller.png";
 import BikeMeAppTALLER from "../../img/BikeMeAppTALLER.png";
+import bici from "../../img/bici.png";
+import estrella from "../../img/estrella.png";
 
 export const MapTaller = () => {
 	return (
@@ -17,12 +19,25 @@ export const MapTaller = () => {
 						<div className="row">
 							<div className="col-6">
 								<p>
-									<img src={BikeMeAppTALLER} />
+									<img className="help" src={BikeMeAppTALLER} />
 								</p>
 							</div>
-							<div className="col-6">izquierda</div>
-							<div className="col-6">derecha</div>
-							<div className="col-6">izquierda</div>
+							<div className="col-6 pt-5">
+								<h2>En camino en 15 min</h2>
+							</div>
+							<div className="col-6">
+								<p>
+									<img className="bici" src={bici} />
+								</p>
+							</div>
+							<div className="col-6 ">
+								<div className="nota">
+									<p>
+										<img className="help " src={estrella} />
+									</p>
+									<h3>7.5</h3>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
