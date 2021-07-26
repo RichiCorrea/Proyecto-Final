467182dac5abbd4e345539b499865019b547ee12
RichiCorrea <richi.correa.f@gmail.com>
2021-07-26 20:19:05 +0000

    guardando bikers

--- src/api/models.py
+++ src/api/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Biker(db.Model):
+    __tablename__ = "bikers"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     bikername = db.Column(db.String(80), unique = True, nullable=False)
@@ -12,7 +13,7 @@ class Biker(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Biker %r>' % self.email
 
     def serialize(self):
         return {
@@ -37,12 +38,12 @@ class Biker(db.Model):
 
 
 class Helper(db.Model):
-    __tablename__ = "helper"
+    __tablename__ = "helpers"
     id = db.Column(db.Integer, primary_key = True)
-    biker_id = db.Column(db.Integer, ForeignKey("biker.id"))
-    biker = relationship(Biker)
-    taller_id = db.Column(db.Integer, ForeignKey("taller.id"))
-    taller = relationship(Taller)
+    biker_id = db.Column(db.Integer, db.ForeignKey("bikers.id"))
+    biker = db.relationship("Biker")
+    taller_id = db.Column(db.Integer, db.ForeignKey("talleres.id"))
+    taller = db.relationship("Taller")
 
     def serialize(self):
         return{
@@ -65,7 +66,7 @@ class Helper(db.Model):
 
 
 class Taller(db.Model):
-    __tablename__ = "taller"
+    __tablename__ = "talleres"
     id = db.Column(db.Integer, primary_key = True)
     tallername = db.Column(db.String(80), unique = True, nullable=False)
     email = db.Column(db.String(80),unique=True)
@@ -74,10 +75,10 @@ class Taller(db.Model):
 
     def serialize(self):
         return{
-            "id" = self.id,
-            "tallername" = self.tallername,
-            "email" = self.email
-            "address" = self.address
+            "id" : self.id,
+            "tallername" : self.tallername,
+            "email" : self.email,
+            "address" : self.address,
         }
     def save(self):
         db.session.add(self)
@@ -91,18 +92,18 @@ class Taller(db.Model):
         db.session.commit()
 
 class Blog(db.Model):
-    __tablename__ = "post"
+    __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key = True)
-    biker_id = db.Column(db.Integer, ForeignKey("biker.id"))
-    biker = relationship(Biker)
-    taller_id = db.Column(db.Integer, ForeignKey("taller.id"))
-    taller = relationship(Taller)
+    biker_id = db.Column(db.Integer, db.ForeignKey("bikers.id"))
+    biker = db.relationship("Biker")
+    taller_id = db.Column(db.Integer, db.ForeignKey("talleres.id"))
+    taller = db.relationship("Taller")
 
     def serialize(self):
         return{
-        "id" = self.id,
-        "bikerid" = self.biker_id,
-        "tallerid" = self.taller_id
+        "id" : self.id,
+        "bikerid" : self.biker_id,
+        "tallerid" : self.taller_id
         }
     def save(self):
         db.session.add(self)
@@ -116,20 +117,20 @@ class Blog(db.Model):
         db.session.commit()
 
 class Comment(db.Model):
-    __tablename__ = "comment"
+    __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key = True)
     comment_text = db.Column(db.String(200))
-    author_id = db.Column(db.Integer, ForeignKey("biker.id"))
-    user = relationship(User)
-    blog_id = db.Column(db.Integer, ForeignKey("blog.id"))
-    blog = relationship(Blog)
+    author_id = db.Column(db.Integer, db.ForeignKey("bikers.id"))
+    user = db.relationship("Biker")
+    blog_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    blog = db.relationship("Blog")
 
     def serialize(self):
         return{
-            "id" = self.id,
-            "commenttext" = self.comment_text,
-            "authorid" = self.author_id,
-            "blogid" = self.blog_id
+            "id" : self.id,
+            "commenttext" : self.comment_text,
+            "authorid" : self.author_id,
+            "blogid" : self.blog_id
         }
     def save(self):
         db.session.add(self)
