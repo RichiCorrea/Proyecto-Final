93919209cd3c29019d737926ce268befc6eef0e0
RichiCorrea <richi.correa.f@gmail.com>
2021-07-27 19:40:40 +0000

    eran corchetes xd

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import BikeMeApp from "../../img/BikeMeApp.png";
@@ -6,6 +6,30 @@ import "../../styles/home.scss";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleClick = () => {
+		const opts = {
+			method: "POST",
+			Headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password
+			})
+		};
+		fetch("https://3000-amber-stingray-xdjmtvct.ws-us11.gitpod.io/api/token", opts)
+			.then(resp => {
+				if (resp.status === 200) return resp.json();
+				else alert("there has been some error");
+			})
+			.then()
+			.catch(error => {
+				console.error("there was an error!!!", error);
+			});
+	};
 
 	return (
 		<div className="Container">
@@ -13,7 +37,7 @@ export const Home = () => {
 				<div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6  text-center">
 					<h1 className="titulo">BikeMeApp</h1>
 					<p>
-						<img className="logo" src={BikeMeApp} />
+						<img src={BikeMeApp} />
 					</p>
 				</div>
 
@@ -30,6 +54,8 @@ export const Home = () => {
 										className="form-control"
 										id=""
 										placeholder="email@Address.com"
+										value={email}
+										onChange={e => setEmail(e.target.value)}
 									/>
 								</div>
 								<div className="col-12">
@@ -41,10 +67,12 @@ export const Home = () => {
 										className="form-control"
 										id="inputPassword"
 										placeholder="password"
+										value={password}
+										onChange={e => setPassword(e.target.value)}
 									/>
 								</div>
 								<div className="col-12">
-									<button type="button" className="btn btn-primary m-3">
+									<button type="button" className="btn btn-primary m-3" onClick={handleClick}>
 										Iniciar esion
 									</button>
 								</div>
@@ -52,11 +80,9 @@ export const Home = () => {
 									<h5>¿Olvidaste tu contraseña?</h5>
 								</div>
 								<div className="col-12">
-									<Link to="/Registro">
-										<button type="button" className="btn btn-primary m-3">
-											Crear cuenta
-										</button>
-									</Link>
+									<button type="button" className="btn btn-primary m-3">
+										Crear cuenta
+									</button>
 								</div>
 							</div>
 						</form>
