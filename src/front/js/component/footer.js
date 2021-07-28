2e79f47306c83c9520c42ef7031f8105f0e4c97e
RichiCorrea <richi.correa.f@gmail.com>
2021-07-28 02:49:29 +0000

    footer icon

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,9 +3,11 @@ import "../../styles/footer.scss";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		{/* <p>
-			¡Las aventuras esperan que giren tus ruedas! <i className="fa fa-bicycle text-black" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p> */}
+		{
+			<p className="text-white bg-dark">
+				¡Las aventuras esperan que giren tus ruedas! <i className="fa fa-bicycle text-black" /> by{" "}
+				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			</p>
+		}
 	</footer>
 );
