e9906ee7ee14d51b03bf940f579858ab7defb1b6
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-07-29 23:50:37 +0000

    commit taller 2.0

--- src/front/js/pages/MapTaller.js
+++ src/front/js/pages/MapTaller.js
@@ -1,61 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/maptaller.scss";
 import { ElMap } from "../component/map";
-import Marker from "react-google-maps/lib/components/Marker";
+import {
+	Marker,
+	InfoWindow,
+	GoogleMap,
+	withScriptjs,
+	withGoogleMap,
+	maps
+} from "react-google-maps/lib/components/Marker";
 import * as TallerData from "/workspace/Proyecto-Final/src/data/Talleres.json";
 
 export const MapTaller = () => {
+	const [selectedTaller, setSelectedTaller] = useState(null);
 	return (
-		<div className="Container">
-			<div className="row  ">
-				<div className="col-md-12 col-lg-6 text-center pt-5">
-					<ElMap>
-						{TallerData.features.map(taller => (
-							<Marker
-								key={taller.properties.TALLER_ID}
-								position={{ lat: taller.geometry.coordinates[0], lng: taller.geometry.coordinates[1] }}
-							
-							onClick={() => {
-								setSelectedTaller(taller);
-							}}	
-							/>
-						))}
+		<>
+			<div className="Container">
+				<div className="row  ">
+					<div className="col-md-12 col-lg-6 text-center pt-5">
+						<ElMap>
+							{TallerData.features.map(taller => (
+								<Marker
+									key={taller.properties.TALLER_ID}
+									position={{
+										lat: taller.geometry.coordinates[0],
+										lng: taller.geometry.coordinates[1]
+									}}
+									onClick={() => {
+										setSelectedTaller(taller);
+									}}
+									icon={{
+										url: "/workspace/Proyecto-Final/src/front/img/BikeMeAppTALLER.png"
+									}}
+								/>
+							))}
 
-						{setSelectedTaller && (
-							<InfoWindow
-							position={{ lat: taller.geometry.coordinates[0], lng: taller.geometry.coordinates[1] }}
-							onCloseClick={() => {
-								setSelectedTaller(null);
-							}} >
-								<div>
-									<h2>{selectedTaller.properties.NAME}</h2>
-									<p>{selectedTaller.properties.DESCRIPTIO}</p>
-									<p>{selectedTaller.propierties.HORARIO}</p>
-								</div>
-							</InfoWindow>
-						)}
-
-					</ElMap>
-				</div>
-				<div className="col-md-12 col-lg-6 text-center pt-5">
-					<div className="card">
-						<ul className="">
-							<li>
-								<h3>San Eugenio 1502, Ñuñoa</h3>
-							</li>
-							<li>
-								<h3>San Eugenio 1502, Ñuñoa</h3>
-							</li>
-							<li>
-								<h3>San Eugenio 1502, Ñuñoa</h3>
-							</li>
-							<li>
-								<h3>San Eugenio 1502, Ñuñoa</h3>
-							</li>
-						</ul>
+							{setSelectedTaller == !null && (
+								<InfoWindow
+									position={{
+										lat: selectedTaller.geometry.coordinates[0],
+										lng: selectedTaller.geometry.coordinates[1]
+									}}
+									onCloseClick={() => {
+										setSelectedTaller(null);
+									}}>
+									<div>
+										<h2>{selectedTaller.properties.NAME}</h2>
+										<p>{selectedTaller.properties.DESCRIPTIO}</p>
+										<p>{selectedTaller.propierties.HORARIO}</p>
+									</div>
+								</InfoWindow>
+							)}
+						</ElMap>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
