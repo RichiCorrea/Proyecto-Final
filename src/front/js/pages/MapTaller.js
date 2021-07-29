638f9e30345a7f2bfe1156f63fda128c8c34891c
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-07-29 22:23:26 +0000

    lista de talleres y talleres con punto en mapa

--- src/front/js/pages/MapTaller.js
+++ src/front/js/pages/MapTaller.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "../../styles/maptaller.scss";
-
-import maptaller from "../../img/maptaller.png";
+import { ElMap } from "../component/map";
+import Marker from "react-google-maps/lib/components/Marker";
+import * as TallerData from "/workspace/Proyecto-Final/src/data/Talleres.json";
 
 export const MapTaller = () => {
 	return (
 		<div className="Container">
 			<div className="row  ">
 				<div className="col-md-12 col-lg-6 text-center pt-5">
-					<p>
-						<img src={maptaller} />
-					</p>
+					<ElMap>
+						{TallerData.features.map(taller => (
+							<Marker
+								key={taller.properties.TALLER_ID}
+								position={{ lat: taller.geometry.coordinates[0], lng: taller.geometry.coordinates[1] }}
+							/>
+						))}
+					</ElMap>
 				</div>
 				<div className="col-md-12 col-lg-6 text-center pt-5">
 					<div className="card">
