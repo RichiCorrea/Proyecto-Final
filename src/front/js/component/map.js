7a496ccded91abfdb913d8a3839679f838df1712
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-07-30 15:06:24 +0000

    talleres funcionando

--- src/front/js/component/map.js
+++ src/front/js/component/map.js
@@ -1,14 +1,66 @@
-import React, { useState } from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import React, { useState, useEffect } from "react";
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
+import * as TallerData from "/workspace/Proyecto-Final/src/data/Talleres.json";
 
 function Map() {
-	return <GoogleMap defaultZoom={10} defaultCenter={{ lat: -33.4369436, lng: -70.634449 }} />;
+	const [selectedTaller, setSelectedTaller] = useState(null);
+
+	useEffect(() => {
+		const listener = e => {
+			if (e.key === "Escape") {
+				setSelectedTaller(null);
+			}
+		};
+		window.addEventListener("keydown", listener);
+
+		return () => {
+			window.removeEventListener("keydown", listener);
+		};
+	}, []);
+
+	return (
+		<GoogleMap defaultZoom={10} defaultCenter={{ lat: -33.4138, lng: -70.5833 }}>
+			{TallerData.features.map(taller => (
+				<Marker
+					key={taller.properties.TALLER_ID}
+					position={{
+						lat: taller.geometry.coordinates[0],
+						lng: taller.geometry.coordinates[1]
+					}}
+					onClick={() => {
+						setSelectedTaller(taller);
+					}}
+					icon={{
+						url: `/workspace/Proyecto-Final/src/front/img/311002.svg`,
+						scaledSize: new window.google.maps.Size(25, 25)
+					}}
+				/>
+			))}
+
+			{selectedTaller && (
+				<InfoWindow
+					onCloseClick={() => {
+						setSelectedTaller(null);
+					}}
+					position={{
+						lat: selectedTaller.geometry.coordinates[0],
+						lng: selectedTaller.geometry.coordinates[1]
+					}}>
+					<div>
+						<h2>{selectedTaller.properties.NAME}</h2>
+						<p>{selectedTaller.properties.DESCRIPTIO}</p>
+						<p>Horario: {selectedTaller.properties.HORARIO}</p>
+						<p>Calificacion: {selectedTaller.properties.CALIFICACION}</p>
+					</div>
+				</InfoWindow>
+			)}
+		</GoogleMap>
+	);
 }
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export const ElMap = () => {
-	const [selectedTaller, setSelectedTaller] = useState(null);
 	return (
 		<div style={{ width: "100vw", height: "100vh" }}>
 			<WrappedMap
--- src/front/js/pages/MapTaller.js
+++ src/front/js/pages/MapTaller.js
@@ -18,40 +18,7 @@ export const MapTaller = () => {
 			<div className="Container">
 				<div className="row  ">
 					<div className="col-md-12 col-lg-6 text-center pt-5">
-						<ElMap>
-							{TallerData.features.map(taller => (
-								<Marker
-									key={taller.properties.TALLER_ID}
-									position={{
-										lat: taller.geometry.coordinates[0],
-										lng: taller.geometry.coordinates[1]
-									}}
-									onClick={() => {
-										setSelectedTaller(taller);
-									}}
-									icon={{
-										url: "/workspace/Proyecto-Final/src/front/img/BikeMeAppTALLER.png"
-									}}
-								/>
-							))}
-
-							{setSelectedTaller == !null && (
-								<InfoWindow
-									position={{
-										lat: selectedTaller.geometry.coordinates[0],
-										lng: selectedTaller.geometry.coordinates[1]
-									}}
-									onCloseClick={() => {
-										setSelectedTaller(null);
-									}}>
-									<div>
-										<h2>{selectedTaller.properties.NAME}</h2>
-										<p>{selectedTaller.properties.DESCRIPTIO}</p>
-										<p>{selectedTaller.propierties.HORARIO}</p>
-									</div>
-								</InfoWindow>
-							)}
-						</ElMap>
+						<ElMap />
 					</div>
 				</div>
 			</div>
